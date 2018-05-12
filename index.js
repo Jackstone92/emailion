4b56acaf1614dad9eb8091f7f5729247c20219f0
Jack Stone <jackstonemedia@gmail.com>
2018-05-12 21:48:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// import express using common.js modules //
+// Currently, Node.js only supports common.js modules - 'import' no support for es2015 modules yet //
+const express = require('express');
+
+// set up application //
+const app = express();
+
+// create a route handler and associate it with a given route //
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// heroku dynamic port from injected environment variables //
+const PORT = process.env.PORT || 5000;
+// instruct Node to listen on port //
+app.listen(PORT);
